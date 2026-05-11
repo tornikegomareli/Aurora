7d60315e5fd94f4f1adb39ca6ecdfbff018ee375
tgomareli <gomarelidevelopment@gmail.com>
2026-05-12 02:07:05 +0400

    compress builders with keypath helper

--- Sources/Aurora/AuroraGlow+Builders.swift
+++ Sources/Aurora/AuroraGlow+Builders.swift
@@ -1,39 +1,16 @@
 import SwiftUI
 
 extension AuroraGlow {
-  public func cornerRadius(_ value: CGFloat) -> Self {
-    var copy = self
-    copy.cornerRadius = value
-    return copy
-  }
+  public func cornerRadius(_ value: CGFloat) -> Self { with(\.cornerRadius, value) }
+  public func borderWidth(_ value: CGFloat) -> Self  { with(\.borderWidth, value) }
+  public func glowSize(_ value: CGFloat) -> Self     { with(\.glowSize, value) }
+  public func speed(_ value: Double) -> Self         { with(\.speed, value) }
+  public func burstsOnAppear(_ value: Bool) -> Self  { with(\.burstsOnAppear, value) }
+  public func burster(_ value: Burster?) -> Self     { with(\.burster, value) }
 
-  public func borderWidth(_ value: CGFloat) -> Self {
+  private func with<T>(_ keyPath: WritableKeyPath<Self, T>, _ value: T) -> Self {
     var copy = self
-    copy.borderWidth = value
-    return copy
-  }
-
-  public func glowSize(_ value: CGFloat) -> Self {
-    var copy = self
-    copy.glowSize = value
-    return copy
-  }
-
-  public func speed(_ value: Double) -> Self {
-    var copy = self
-    copy.speed = value
-    return copy
-  }
-
-  public func burstsOnAppear(_ value: Bool) -> Self {
-    var copy = self
-    copy.burstsOnAppear = value
-    return copy
-  }
-
-  public func burster(_ value: Burster?) -> Self {
-    var copy = self
-    copy.burster = value
+    copy[keyPath: keyPath] = value
     return copy
   }
 }
